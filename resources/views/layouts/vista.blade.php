9363dd39685505f536b714044a9602cb5cf67864
roman08 <rmcentinela@gmail.com>
2015-09-10 11:45:13 -0500

    se carga menu de la aplicacion

--- resources/views/layouts/vista.blade.php
+++ resources/views/layouts/vista.blade.php
@@ -7,13 +7,18 @@
   </title>
    <!-- css bootstrap -->
   {!! Html::style('css/bootstrap.css') !!}
+  {!! Html::style('css/menu.css') !!}
 
     <!-- JQuery -->
     {!! Html::script('js/jquery-1.11.3.min.js') !!}
 
     <!-- JS Bootstrap -->
     {!! Html::script('js/bootstrap.min.js') !!}
-
+ @section('navbar')
+      <div class="row">
+        @include('menu.menu')
+      </div>
+    @show
 </head>
 <body>
  @yield('content')
@@ -21,7 +26,6 @@
     <div class="panel-body">
         <h3>Series</h3>
 
-              
     </div>
   </div>
 
